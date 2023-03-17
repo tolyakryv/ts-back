dbf454c1c6bcdade771d65c85b95bb7c809af33b
tolyakryv <90652628+tolyakryv@users.noreply.github.com>
2023-03-17 15:52:22 +0200

    changes type

---
+++ src/middleware/test.ts
@@ -0,0 +1,2 @@
+// connect to  db sql local port=3000 in nodejs?
+
--- src/types/type.ts
+++ src/types/type.ts
@@ -1,9 +1,9 @@
 import { IsDefined, IsString } from 'class-validator';
 export class BodyPost {
     @IsDefined()
-    city: string;
+    city: string | undefined;
     
-    @IsDefined()@IsString()
-        god: string;
+    @IsDefined()
+    checked: boolean | undefined;
     
 }
